1eb27198a9cb15cb977c9fca628e38d5637a97c0
Alexey <temnikovalx@icloud.com>
2023-06-12 20:00:34 +0300

    added edit to profile

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           path="/*"
           element={
             <ProtectedRoute>
-              <Home />{" "}
+              <Home />
             </ProtectedRoute>
           }
         />
