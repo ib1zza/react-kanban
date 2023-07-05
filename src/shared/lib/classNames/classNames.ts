ef32b55ddf0d5d59c9ce36ac6f9213821662924a
Alexey <temnikovalx@icloud.com>
2023-07-05 13:34:22 +0300

    fixed classNames.ts

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -4,12 +4,12 @@ export function classNames(
   cls: string,
   mods: Mods = {},
   additional: string[] = []
-) {
+): string {
   return [
     cls,
     ...additional.filter(Boolean),
-    Object.entries(mods)
-      .filter(([cls, value]) => !!value)
-      .map(([cls]) => cls),
+    ...Object.entries(mods)
+      .filter(([_, value]) => Boolean(value))
+      .map(([className]) => className),
   ].join(" ");
 }
