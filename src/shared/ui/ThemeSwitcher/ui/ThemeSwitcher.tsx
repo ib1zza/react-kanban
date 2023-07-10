4d0f86c5c330a3c4095628e94a4bf08f244a85eb
Alexey <temnikovalx@icloud.com>
2023-07-10 11:42:20 +0300

    changing design

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -3,7 +3,7 @@ import { useTheme } from "../../../../app/providers/theme/lib/useTheme";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon, faSun } from "@fortawesome/free-regular-svg-icons";
 import { Theme } from "../../../../app/providers/theme/lib/ThemeContext";
-import s from "./ThemeSwitcher.module.scss";
+
 import { classNames } from "../../../lib/classNames/classNames";
 interface ThemeSwitcherProps{
     className?: string
@@ -11,7 +11,7 @@ interface ThemeSwitcherProps{
 const ThemeSwitcher = ({className} : ThemeSwitcherProps) => {
     const { toggleTheme, theme } = useTheme();
     return (
-        <button className={classNames(s.button__theme,{}, 
+        <button className={classNames('',{}, 
             [className as string])} onClick={toggleTheme}>
             {theme === Theme.LIGHT ? (
                 <FontAwesomeIcon icon={faSun} />
