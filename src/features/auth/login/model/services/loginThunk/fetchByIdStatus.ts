6800430d5089abe601aa46d5655e70161d21469c
Alexey <temnikovalx@icloud.com>
2023-09-24 18:20:36 +0300

    fixed fetchByIdStatus getting error

---
+++ src/features/auth/login/model/services/loginThunk/fetchByIdStatus.ts
@@ -0,0 +1,22 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+import {
+    browserLocalPersistence, getAuth, inMemoryPersistence, signInWithEmailAndPassword,
+} from 'firebase/auth';
+
+export const fetchByIdStatus = createAsyncThunk(
+    'user/fetchByIdStatus',
+    async (params: {email: string, password: string, remember: boolean}, { rejectWithValue }) => {
+        const auth = getAuth();
+        if (params.remember) {
+            await getAuth().setPersistence(browserLocalPersistence);
+        } else {
+            await getAuth().setPersistence(inMemoryPersistence);
+        }
+        return signInWithEmailAndPassword(auth, params.email, params.password).then(async (userCredential) => {
+            const { user } = userCredential;
+
+            return user;
+        }).catch((error: any) => rejectWithValue(error.code));
+    },
+);
